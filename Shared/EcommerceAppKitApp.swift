f5f11ef58f4c934256f87da0e137d6ee52d24e56
WAAD ALSAIF <50026099+WaadAlsaif@users.noreply.github.com>
2022-08-22 18:25:42 +0300

    Initial Commit

---
+++ Shared/EcommerceAppKitApp.swift
@@ -0,0 +1,12 @@
+
+
+import SwiftUI
+
+@main
+struct EcommerceAppKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/View/OnBoardingPage.swift
@@ -0,0 +1,74 @@
+
+import SwiftUI
+
+let customFont = "Raleway-Regular"
+
+struct OnBoardingPage: View {
+
+    @State var showLoginPage: Bool = false
+    var body: some View {
+        
+        VStack(alignment: .leading){
+            
+            Text("Find your\nGadget")
+                .font(.custom(customFont, size: 55))
+           
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+            
+            Image("OnBoard")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            
+            Button {
+                withAnimation{
+                    showLoginPage = true
+                }
+            } label: {
+             
+                Text("Get started")
+                    .font(.custom(customFont, size: 18))
+                    .fontWeight(.semibold)
+                    .padding(.vertical,18)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .shadow(color: Color.black.opacity(0.1), radius: 5, x: 5, y: 5)
+                    .foregroundColor(Color("Purple"))
+            }
+            .padding(.horizontal,30)
+       
+            .offset(y: getRect().height < 750 ? 20 : 40)
+            
+            Spacer()
+        }
+        .padding()
+        .padding(.top,getRect().height < 750 ? 0 : 20)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(
+        
+            Color("Purple")
+        )
+        .overlay(
+        
+            Group{
+                if showLoginPage{
+                    LoginPage()
+                        .transition(.move(edge: .bottom))
+                }
+            }
+        )
+    }
+}
+
+struct OnBoardingPage_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardingPage()
+    }
+}
+
+extension View{
+    func getRect()->CGRect{
+        return UIScreen.main.bounds
+    }
+}
